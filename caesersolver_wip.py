2194e9de8ff767f37a87db7506661f351eefe84b
shinrl1 <shinrl1@student.op.ac.nz>
2020-07-27 05:00:45 +1200

    shift code calculated

---
+++ caesersolver_wip.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 22 23:28:03 2020
+
+@author: Bex.0
+"""
+
+
+from collections import Counter
+#plain text to perform cipher on
+input_string = "abccc"
+
+#predetermined monogram frequency table
+probability = {
+        "a": 0.08167,
+        "b": 0.01492,
+        "c": 0.02782
+        }
+#print monogram frequency probabilities
+for i in probability:
+    print(i, probability[i])
+#find total length of phrase    
+length = len(input_string)
+print("length of input string =", length)
+
+#Dictionary lookup to obtain monogram freq (needs to go in for loop)
+letter = "a"
+if letter in probability:
+    print('probability of', letter, probability[letter])
+    
+
+for i in range(len(input_string)):
+    position_number = i
+    print("position number", position_number)
+    position_character = input_string[i]
+    print("position character", position_character)
+    char_count = input_string.count(position_character)
+    print("amount of ", position_character, "in", input_string, "is", char_count)
+    print(position_character)
+    if position_character in probability:
+        prob = probability[position_character]
+        print('probability of', position_character, prob)
+    shift = (char_count-((prob*length)**2))/(prob*length)
+    print("shift code", shift)
+    
+    
+    #caeser cipher 
+    newval = chr((ord(position_character)+shift)%26) + ord(99)
+        
+
+ 
+
+
+
+        
+    
+    
+    
+#create dictionary with monogram frequencies
+#count length of input phrase
+#for each alphabetical character in input_string
+    # find frequency of chaar
+    #find alphabetical character's corresponding monogram frequency in dictionary i.e if letter is A probability is 0.08167
+    #chi-squared = (letter freq in phrase - (length * monogramfreq)**2)/2
+    #multiply chi-squared by 1000
+#shift key = lowest chi-squared value 
+    
+#once the shift key is obtained:
+    #foreach value in input_string
+    #add shiftkey values to alphabet character
