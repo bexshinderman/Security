e576a17e449284c31c40ecfb0fd642b0e0959be8
shinrl1 <shinrl1@student.op.ac.nz>
2020-08-03 02:03:12 +1200

    pycrpto unfinished

--- pycrypto.py
+++ pycrypto.py
@@ -8,7 +8,7 @@ Created on Mon Aug  3 01:24:01 2020
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto import Random
-
+import base64
 #key represents 26 uppercase letters - substitution cipher - 
 key = open('sub_key.txt', 'r').read()
 data = open('sub_ciphertext.txt', 'r').read()
@@ -17,13 +17,13 @@ print(type(key))
 print(data)
 print(type(data))
 import os
-def decryption(data):
-
-    key = open('sub_key.txt', 'r').read()
-	DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip('{')
+def decryption(encryptedString):
+	PADDING = '{'
+	DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(PADDING)
 	#Key is FROM the printout of 'secret' in encryption
 	#below is the encryption.
-	encryption = open('sub_ciphertext.txt', 'r').read()
+	encryption = encryptedString
+	key = open('sub_key.txt', 'r').read()
 	cipher = AES.new(key)
 	decoded = DecodeAES(cipher, encryption)
 	print(decoded)
